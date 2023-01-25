93f407ec34ff34276ddeb774f375a7f0928f3dd8
Samarthya Singh <samarthya55@gmail.com>
2023-01-25 10:31:09 +0530

    Action prepare callback

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -1,4 +1,3 @@
-import { nanoid } from '@reduxjs/toolkit'
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { postAdded } from './postsSlice'
@@ -14,10 +13,7 @@ export const AddPostForm = () => {
 
   function submitHandler() {
     if(title && content) {
-      dispatch(postAdded({
-        id: nanoid(),
-        title, content
-      }));
+      dispatch(postAdded(title, content));
     }
 
     setTitle('');
